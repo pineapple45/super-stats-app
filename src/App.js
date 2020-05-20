6ff83f0d05b01b8abfacc865f47233c5fec93c85
pineapple45 <gomraanmol@gmail.com>
2020-05-20 20:30:01 +0530

    i" commited changes made "

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
+import Home from './containers/Home/Home';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <Home />
+    )
+  }
 }
-
 export default App;
+
+
+// access_token = 610683779792139
